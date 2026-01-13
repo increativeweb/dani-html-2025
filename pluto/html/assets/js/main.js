560fd8cb5546d5e8bfdcc1628daf1a767128e374
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-01-13 14:46:09 +0530

    Update Header in Pluto HTML

--- pluto/html/assets/js/main.js
+++ pluto/html/assets/js/main.js
@@ -4,7 +4,26 @@
 var $ = jQuery.noConflict();
 
 jQuery(document).ready(function ($) {
-    $('[data-bs-toggle="tooltip"]').tooltip();
 
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 50) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 50) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+        $('.navbar-toggler').on('click', function () {
+            $(".main-header").toggleClass('is-visible');
+            $('body').toggleClass('overflow-hidden');
+            $(this).toggleClass('is-visible');
+        });
+    }
 
 });
