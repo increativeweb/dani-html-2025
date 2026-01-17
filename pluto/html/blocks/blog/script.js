e602e9e39ff25fc8933b70d66cfb5785e9d40d46
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-01-17 14:49:08 +0530

    Update Blog Details Page in Pluto

--- pluto/html/blocks/blog/script.js
+++ pluto/html/blocks/blog/script.js
@@ -26,3 +26,52 @@ jQuery(function ($) {
         });
     });
 });
+
+// Post Slider
+if (jQuery('.post-splide').length) {
+    const slider = new Splide('.post-splide', {
+        type: 'slide',
+        perPage: 3,
+        perMove: 1,
+        pagination: true,
+        arrows: false,
+        gap: '56px',
+        classes: {
+            pagination: 'splide__pagination is-dark'
+        },
+        breakpoints: {
+            1200: {
+                perPage: 3,
+            },
+            992: {
+                perPage: 2,
+                padding: '1rem',
+                gap: '1.5rem',
+                autoWidth: true,
+            },
+            767: {
+                perPage: 1,
+                autoWidth: true,
+                drag: true,
+            }
+        }
+    });
+
+    slider.on('mounted', function () {
+        const slideCount = slider.length; // total slides
+        if (window.innerWidth > 1400) {
+            if (slideCount <= 3) {
+                // Disable all movement
+                slider.options = {
+                    arrows: false,
+                    pagination: false,
+                    drag: false,
+                    keyboard: false,
+                    flickPower: 0,
+                };
+            }
+        }
+    });
+
+    slider.mount();
+}
