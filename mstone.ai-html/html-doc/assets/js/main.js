c63c8fedd832cc77d84c1eb6b628ca1e1d32916b
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-02-04 19:25:58 +0530

    Update Mstone Doc HTML

---
+++ mstone.ai-html/html-doc/assets/js/main.js
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function ($) {
+    if ($('.main-header').length) {
+        $('.navbar-toggler').on('click', function () {
+            $(".main-sidebar").toggleClass('is-visible');
+            $('body').toggleClass('overflow-hidden');
+            $(this).toggleClass('is-visible');
+        });
+    }
+});
+
+if ($('.main-sidebar').length) {
+    $(document).on('click', '.menu-list-collapsed .collapse-title', function (e) {
+        e.preventDefault();
+
+        var $this = $(this).closest('.menu-list-collapsed');
+        var $menu = $this.find('.collapse-menu-block');
+
+        $this.toggleClass('active');
+        $menu.stop(true, true).slideToggle(300);
+        // Close other open menus
+        // $('.menu-list-collapsed').not($this)
+        //     .removeClass('active')
+        //     .find('.collapse-menu-block')
+        //     .stop(true, true)
+        //     .slideUp(300);
+
+        // // Toggle current menu
+        // if ($this.hasClass('active')) {
+        //     $this.removeClass('active');
+        //     $menu.stop(true, true).slideUp(300);
+        // } else {
+        //     $this.addClass('active');
+        //     $menu.stop(true, true).slideDown(300);
+        // }
+    });
+
+}
