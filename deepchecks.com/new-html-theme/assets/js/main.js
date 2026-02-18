50cf52ac9dd4a57ed863e7059650999023587e3c
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-02-18 18:50:29 +0530

    Update Header Sub menu in New Page

--- deepchecks.com/new-html-theme/assets/js/main.js
+++ deepchecks.com/new-html-theme/assets/js/main.js
@@ -36,6 +36,16 @@ jQuery(document).ready(function ($) {
                 $(".header-nav-wrap").removeClass("fixed-header");
             }
         });
+        
+        if ($('li.menu-item-has-children').length) {
+            $('li.menu-item-has-children > a').after('<i class="arrow"></i>');
+        }
+        $('li.menu-item-has-children .arrow').on('click', function (event) {
+            event.preventDefault();
+            $(this).toggleClass('is-active');
+            $(this).parent().find('.sub-menu').first().slideToggle(300);
+
+        });
     }
 
     $('li.menu-item-has-children').hover(function () {
