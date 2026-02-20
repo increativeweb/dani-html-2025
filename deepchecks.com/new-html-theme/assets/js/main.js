279063b3133c3774ec3479a7cf3f5f7b12ea20d1
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-02-21 00:23:53 +0530

    Update JS

--- deepchecks.com/new-html-theme/assets/js/main.js
+++ deepchecks.com/new-html-theme/assets/js/main.js
@@ -137,6 +137,37 @@ jQuery(document).ready(function ($) {
                 }
             ]
         });
+    }       
+    if ($('.home-post-slider').length) {
+        $('.home-post-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            accessibility: false,
+            infinite: false,
+            draggable: false,
+            variableWidth: true,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: true
+                    }
+                }
+            ]
+        });
     }
     if ($('.post-slider').length) {
         $('.post-slider').slick({
