4eb6a8e4f17c6d757ef23104a6c010e2b1513e21
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-04-06 23:01:34 +0530

    Update Hero Section in New Home Page in Hud.io

---
+++ hud.io/html-wp-2.0/blocks/hero-2.0/script.js
@@ -0,0 +1,35 @@
+if (jQuery('.hero-thumb-slider-20-block').length) {
+    var main = new Splide("#hero-img-slider", {
+        type: "loop",
+        lazyLoad: 'nearby',
+        pagination: false,
+        arrows: false,
+        autoWidth: true,
+        rewind: false,
+        gap: '15px',
+        isNavigation: false,
+        // autoplay: true,
+        // interval: 8000,
+        breakpoints: {
+            991: {
+                pagination: true,
+                "destroy": true
+            },
+        }
+    });
+
+    var thumbnails = new Splide("#thumb-info-slider", {        
+        direction: "ttb",
+        height: "100%",
+        perPage: 3,
+        wheel: false,
+        arrows: false,
+        isNavigation: true,
+        pagination: false,
+        drag: false, // Disables drag functionality
+    });
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+}
